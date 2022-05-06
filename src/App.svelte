fab8658b7bf14fa08a7c651bde181cbddd5b3f06
Ant3ney <46980442+Ant3ney@users.noreply.github.com>
2022-05-05 22:37:07 -0700

    Add prettier cli suppoprt

--- src/App.svelte
+++ src/App.svelte
@@ -1,56 +1,57 @@
 <script>
-	import DogToy from './DogToy.svelte';
-	export let name;
-	export let count = 0;
-	function add(){
-		count++;
-	}
-	const src='/438526.png';
-	let string = `this string contains some <strong>HTML!!!</strong>`;
+  import DogToy from "./DogToy.svelte";
+  export let name;
+  export let count = 0;
+  function add() {
+    count++;
+  }
+  const src = "/dog.jpg";
+
+  let string = `this string contains some <strong>HTML!!!</strong>`;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<div class="mony-container">
-		<h2>You have </h2>
-		<p >${count}</p>
-		<img alt='a dog' src={src}>
-		<button on:click={add}>Add</button>
-		<p>{@html string}</p>
-	</div>
-	<DogToy />
+  <h1>Hello {name}!</h1>
+  <div class="mony-container">
+    <h2>You have</h2>
+    <p>${count}</p>
+    <img alt="a dog" {src} />
+    <button on:click={add}>Add</button>
+    <p>{@html string}</p>
+  </div>
+  <DogToy />
 </main>
 
 <style>
-	.mony-container{
-		display: flex;
-		flex-direction: column;
-	}
-	.mony-container img{
-		width: 10rem;
-		height: auto;
-		margin: 1rem auto 0rem auto;
-	}
-	.mony-container button{
-		margin: 1rem auto 0rem auto;
-	}
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  .mony-container {
+    display: flex;
+    flex-direction: column;
+  }
+  .mony-container img {
+    width: 10rem;
+    height: auto;
+    margin: 1rem auto 0rem auto;
+  }
+  .mony-container button {
+    margin: 1rem auto 0rem auto;
+  }
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
